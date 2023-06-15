d228283f25fc39e13d57159ce85d90a91c051b93
Thatisus <thatisus@gmail.com>
2023-06-15 15:53:04 -0500

    Added Day 1 project
    Day one project was to create a Band Name Generator.

---
+++ band_name_generator.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the Band Name Generator 2000!")
+
+print("What city did you grow up in?")
+city = input()
+
+print("Tell us the name of a pet you had!")
+pet = input()
+
+print("Your band name is " + city + " " + pet + "!")
